3be30dcc4e7d926e75572b00759a050501f4b24e
Ricardo Valério <jricvalerio@gmail.com>
2015-05-20 01:01:20 +0100

    :rocket:

--- 00 - Zero To Hero - reading the docs/whats_your_name.py
+++ 00 - Zero To Hero - reading the docs/whats_your_name.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 
-
 def main():
+    name = whats_your_name()
+    print("Hello,", name)
+    print(name, "let's calculate the area of a triangle")
 
-	name = whats_your_name()
-	print("Hello, " + name)
-	print(name + " let's calculate the area of a triangle")
+    width = int(input("Enter the width: "))
+    height = int(input("Now enter the height: "))
+    area = area_of_a_triangle( width = width, height = height)
 
-	width  = int(input("Enter the width: "))
-	height = int(input("Now enter the height: "))
-	area   = area_of_a_triangle(width, height)
+    print("BTW, just to say first that I have %d cats" % 3)
 
-	print("BTW, just to say first that I have %d cats" % 3)
-
-	print("The area of the triangle is %.1f cms squared" % area)
-
+    print("The area of the triangle is %.1f cms squared" % area)
 
 # function that asks a person's name and returns it to print it
 def whats_your_name():
-	name = input("Enter your name: ")
-	return name
+    name = input("Enter your name: ")
+    return name
+
 
 # function that calculate the area of a triangle
 def area_of_a_triangle(width, height):
-	return width * height / 2
-
+    return width * height / 2
 
 
 if __name__ == '__main__':
-	main()
+    main()
+
